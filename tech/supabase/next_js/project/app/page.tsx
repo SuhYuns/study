5d72346e6694fa00434aeacca9fbe36cb7beca27
yan9hwa <104848737+SuhYuns@users.noreply.github.com>
2025-02-19 17:26:51 +0900

    update

---
+++ tech/supabase/next_js/project/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-900 w-full h-10">
+      <div className="text-white">NEXT group <span>Inner System</span></div>
+    </div>
+  );
+}
